4d07cb797680f37b3bb666ca8ec8a9a317c3a708
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2019-01-21 07:33:20 +0100

    Poprawki

--- version-ts-in-babel/webpack.js
+++ version-ts-in-babel/webpack.js
@@ -1,10 +1,9 @@
 var path = require('path');
 
 module.exports = {
-    // Change to your "entry-point".
     entry: './src/index.jsx',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist/static'),
         filename: 'index.js'
     },
     resolve: {
@@ -12,7 +11,6 @@ module.exports = {
     },
     module: {
         rules: [{
-            // Include ts, tsx, js, and jsx files.
             test: /\.(ts|js)x?$/,
             exclude: /node_modules/,
             loader: 'babel-loader',
